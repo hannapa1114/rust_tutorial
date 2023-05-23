013f94e30569b0d2b3344d5add47378c6de5c8b9
hannapa1114 <flexaveler@gmail.com>
2023-05-23 18:53:33 +0900

    add intro

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,25 +1,37 @@
 fn main() {
     // another_function(5, 6);
 
-    let x = 5;
+    // let x = 5;
 
-    let y = {
-        let x = 3;
-        x + 1
-    };
+    // let y = {
+    //     let x = 3;
+    //     x + 1
+    // };
 
-    let z = five();
+    // let z = five();
 
-    println!("The value of y is: {}", y);
-    println!("The value of z is: {}", z);
+    // println!("The value of y is: {}", y);
+    // println!("The value of z is: {}", z);
+
+    let x = plus_one(5);
+
+    println!("value of x is: {} ", x);
 }
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-}
+// fn another_function(x: i32, y: i32) {
+//     println!("The value of x is: {}", x);
+//     println!("The value of y is: {}", y);
+// }
 
 fn five() -> i32 {
     // 세미콜론을 쓰지 않으면 반환값
     5
+}
+
+// fn plus_one(x: i32) -> i32 {
+//     x + 1; <- 반환값이 없어 에러 발생
+// }
+
+ fn plus_one(x: i32) -> i32 {
+    x + 1
 }
---
+++ intro.md
@@ -0,0 +1,26 @@
+```
+$ cargo --version
+```
+
+```
+$ cargo new hello_cargo --bin
+$ cd hello_cargo
+```
+
+```
+$ cargo build
+```
+
+```
+$ cargo run
+```
+
+```
+$ cargo check
+```
+
+```
+$ cargo build --release
+```
+
+(cargo docs)[https://doc.rust-lang.org/cargo/]
